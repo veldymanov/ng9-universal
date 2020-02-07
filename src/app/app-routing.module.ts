764daf08075e3d08d9385f4e9c3d4dbc3acad021
aveldymanov <aveld@softserveinc.com>
2020-02-07 15:37:45 +0200

    some routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,11 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     component: HomeComponent
   },
   {
